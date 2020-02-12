521d348ceaecd250db251c47f57d41812994f7f0
Sheba1990 <57401985+Sheba1990@users.noreply.github.com>
2020-02-12 14:46:49 +0300

    Full study of a factory pattern

--- src/net/patterns/factory/Program.java
+++ src/net/patterns/factory/Program.java
@@ -2,9 +2,21 @@ package net.patterns.factory;
 
 public class Program {
     public static void main(String[] args) {
-        DeveloperFactory developerFactory = new JavaDeveloperFactory();
+        DeveloperFactory developerFactory = createDeveloperBySpeciality("php");
         Developer developer = developerFactory.createDeveloper();
-
         developer.writeCode();
+
+    }
+
+    static DeveloperFactory createDeveloperBySpeciality(String specialty) {
+        if (specialty.equalsIgnoreCase("java")) {
+            return new JavaDeveloperFactory();
+        } else if (specialty.equalsIgnoreCase("c++")) {
+            return new CppDeveloperFactory();
+        } else if (specialty.equalsIgnoreCase("php")) {
+            return new PhpDeveloperFactory();
+        } else {
+            throw new RuntimeException(specialty + " is unknown specialty");
+        }
     }
 }
